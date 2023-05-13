44cf0399b864d0011afeceeeca5c77a5590210c1
henry sathyan <henrysathyan@gmail.com>
2023-05-13 20:18:39 +0530

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="app">
+     <div className='app-header'>
+        <span className='header-title'>ReactOne</span>
+        <span>Home</span>
+        <span>Usage</span>
+        <span>Settings</span>
+        <span>Logout</span>
+     </div>
+     <div className='app-body'>
+        <div className='app-list'>
+          <div className='list-item'>
+            <hr />
+            <div className='list-title'>
+              <h4>My title One</h4>
+            </div>
+            <div className='list-descr'>
+              This is long
+            </div>
+            <div className='list-label'>
+              <span>Label 1</span>
+              <span>Label 2</span>
+              <span>Label 3</span>
+              
+            </div>
+            <hr />
+          </div>
+
+        </div>
+     </div>
+
+     <div className='app-footer'>Copyright ReactOne by Henry. All Rights Reserved </div>
+
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
